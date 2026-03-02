4ba42841e74b1264c8dc1c4ad8be9d7a7e0b96db
Seth Horsley <github.dvxhi@sl.isethi.com>
2026-03-02 15:06:42 -0500

    Update snet to v0.1.3

--- Formula/snet.rb
+++ Formula/snet.rb
@@ -1,31 +1,31 @@
 class Snet < Formula
-  desc 'Secure HTTPS tunnels from localhost to public URLs'
-  homepage 'https://github.com/sethhorsley/snet-cli'
-  version '0.1.3'
-  license 'MIT'
+  desc "Secure HTTPS tunnels from localhost to public URLs"
+  homepage "https://github.com/sethhorsley/snet-cli"
+  version "0.1.3"
+  license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/sethhorsley/snet-cli/releases/download/v0.1.3/snet-darwin-arm64.tar.gz'
-      sha256 '2d5050c41e41967fabe61f8ad6f62f4e54b0bb821391e3752229dc87e7417777'
+      url "https://github.com/sethhorsley/snet-cli/releases/download/v0.1.3/snet-darwin-arm64.tar.gz"
+      sha256 "2d5050c41e41967fabe61f8ad6f62f4e54b0bb821391e3752229dc87e7417777"
     else
-      url 'https://github.com/sethhorsley/snet-cli/releases/download/v0.1.1/snet-darwin-amd64.tar.gz'
-      sha256 'f13137d137488a51bd7bac2c07c554746dddfa5418afd010d4569ff864c860d6'
+      url "https://github.com/sethhorsley/snet-cli/releases/download/v0.1.3/snet-darwin-amd64.tar.gz"
+      sha256 "12ff46ecbefd069e3dccd58942ac29e7d8c4cec7b7dd4156c05ed53775a4795f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url 'https://github.com/sethhorsley/snet-cli/releases/download/v0.1.1/snet-linux-arm64.tar.gz'
-      sha256 'c2eeadf5488d6d316f2a19c6dff68893637309e2eee98dc9262e8a2906025bfe'
+      url "https://github.com/sethhorsley/snet-cli/releases/download/v0.1.3/snet-linux-arm64.tar.gz"
+      sha256 "32ed1c41a92946daa3b0f9acc73767a1be51c748da10b26c35c18d002fb9be22"
     else
-      url 'https://github.com/sethhorsley/snet-cli/releases/download/v0.1.1/snet-linux-amd64.tar.gz'
-      sha256 '3280fff4ca9fa141a3ac7b601d8defe496267566147b645c797350c38b7d576b'
+      url "https://github.com/sethhorsley/snet-cli/releases/download/v0.1.3/snet-linux-amd64.tar.gz"
+      sha256 "7aa073f74903f9094d55c4911626873674de5cf158d9a3d9d35b82c525999248"
     end
   end
 
   def install
-    bin.install 'snet'
+    bin.install "snet"
   end
 
   test do
